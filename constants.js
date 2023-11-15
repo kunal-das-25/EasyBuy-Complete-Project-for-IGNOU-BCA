0c3acb2a15cd82de909fd7f0d6b570d6e15a4fc6
Kunal Das <87944033+kunal-das-25@users.noreply.github.com>
2023-11-15 09:14:30 +0530

    Delete constants.js

--- constants.js
+++
@@ -1,7 +0,0 @@
-export const THEMES = {
-  LIGHT: 'light',
-  DARK: 'dark'
-};
-export const lightTheme = (theme) => theme.palette.mode === 'light';
-export const secondarySideBarWidth = 215;
-export const secondarySideBarGap = 80;
